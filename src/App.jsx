aa892afae54848ffd693a05fc4d6e6e7f7d49a1e
marvellous <comlaf2001@gmail.com>
2024-07-22 21:59:48 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Dashboard from './components/Dashboard'
+import LandingPage from './components/LandingPage'
+import Navbar from './components/Navbar'
+import Settings from './components/Settings'
+
+
+const App = () => {
+  return (
+    <div>
+      <BrowserRouter>
+        <Navbar/>
+        <Routes>
+          <Route index element={<LandingPage/>}></Route>
+          <Route path='components/Dashboard.jsx' element={<Dashboard/>}></Route>
+          <Route path='components/Dashboard.jsx' element={<Settings/>}></Route>
+        </Routes>
+      </BrowserRouter>
+    </div>
+  )
+}
+
+export default App
