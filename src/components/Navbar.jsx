10108ae98fe10132e124b8ddb9810d824437f1b4
marvellous <comlaf2001@gmail.com>
2024-07-24 15:04:08 +0100

    dashboard navbar added by marvellous

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,29 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import profileImg from './profile.png'
+import { FaSortDown } from "react-icons/fa";
 
 const Navbar = () => {
-  return <div>hello world</div>;
+  return (
+    <nav className="dashboard-nav">
+      <div className="left">
+        <Link className="link">Home</Link>
+        <Link className="link">Organization</Link>
+        <Link className="link">Staff</Link>
+        <Link className="link">Patients</Link>
+        <Link className="link">Appointments</Link>
+        <Link className="link">Account</Link>
+        <Link className="link">Reports</Link>
+        <Link className="link">Settings</Link>
+        <Link className="link">Help</Link>
+      </div>
+      <div className="right">
+        <img src={profileImg} alt="" />
+        <FaSortDown className="dashboard-icon"/>
+      </div>
+    </nav>
+  )
+      ;
 };
 
 export default Navbar;
