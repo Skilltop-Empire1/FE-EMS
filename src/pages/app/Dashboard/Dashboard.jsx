5f131ff2c7b6242ddecbc018afb5149a65166a66
marvellous <comlaf2001@gmail.com>
2024-08-03 12:39:20 +0100

    changes made

--- src/pages/app/Dashboard/Dashboard.jsx
+++ src/pages/app/Dashboard/Dashboard.jsx
@@ -49,37 +49,37 @@ const Dashboard = () => {
       <div className={styles.content}>
         <div className={styles.tableContainer}>
           <h2>Recent Appointments</h2>
-          <table>
-            <thead>
+          <table className={styles.table}>
+            <thead className={styles.thead}>
               <tr>
-                <th>Patients</th>
-                <th>Consulting Doctor</th>
-                <th>Date of Appointment</th>
-                <th>Practice</th>
-                <th>Organisation</th>
+                <th className={styles.th}>Patients</th>
+                <th className={styles.th}>Consulting Doctor</th>
+                <th className={styles.th}>Date of Appointment</th>
+                <th className={styles.th}>Practice</th>
+                <th className={styles.th}>Organisation</th>
               </tr>
             </thead>
             <tbody>
               <tr>
-                <td></td>
-                <td>Doctor A</td>
-                <td>11/15/2023</td>
-                <td>Practice A</td>
-                <td>Organization A</td>
+                <td className={styles.td}></td>
+                <td className={styles.td}>Doctor A</td>
+                <td className={styles.td}>Doctor A</td>
+                <td className={styles.td}>11/15/2023</td>
+                <td className={styles.td}>Practice A</td>
               </tr>
               <tr>
-                <td></td>
-                <td>Doctor A</td>
-                <td>11/01/2023</td>
-                <td>Practice A</td>
-                <td>Organization A</td>
+                <td className={styles.td}></td>
+                <td className={styles.td}>Doctor A</td>
+                <td className={styles.td}>11/01/2023</td>
+                <td className={styles.td}>Practice A</td>
+                <td className={styles.td}>Organization A</td>
               </tr>
               <tr>
-                <td>Sam Johnson</td>
-                <td></td>
-                <td>11/16/2023</td>
-                <td>Practice A</td>
-                <td>Organization A.</td>
+                <td className={styles.td}>Sam Johnson</td>
+                <td className={styles.td}></td>
+                <td className={styles.td}>11/16/2023</td>
+                <td className={styles.td}>Practice A</td>
+                <td className={styles.td}>Organization A.</td>
               </tr>
             </tbody>
           </table>
