8bb83968fff520f0165c18104e43e10fb7bdbbad
eedreese <idbalorg@gmail.com>
2024-08-25 08:10:59 +0100

    created a reusable table for all pages

--- src/pages/app/Appointments/Appointments.jsx
+++ src/pages/app/Appointments/Appointments.jsx
@@ -1,7 +1,61 @@
-import React from "react";
+import React, { useState } from "react";
+import style from "./Appointment.module.css";
+import { initialData, table } from "./data";
+import { MODAL_TYPES, useModal } from "../../../context/ModalContext";
 
-function Appointments() {
-  return <div>Appointments</div>;
+import Table from "../../../components/dataTable/Table";
+
+function Organizations() {
+  const [data, setData] = useState(initialData);
+  const { openModal } = useModal();
+
+  const handleSearch = (event) => {
+    const filteredData = data.filter((item) =>
+      item.username.toLowerCase().includes(event.target.value.toLowerCase())
+    );
+    setData(filteredData);
+    setCurrentPage(1);
+  };
+
+  const renderRow = (item) => (
+    <>
+      <td>{item.patient}</td>
+      <td>{item.consultingDoctor}</td>
+      <td>{item.dateOfAppointment}</td>
+      <td>{item.timeOfAppointment}</td>
+      <td>{item.reason}</td>
+      <td>{item.practice}</td>
+      <td>{item.organization}</td>
+    </>
+  );
+
+  return (
+    <div className={style.container}>
+      <div>
+        <h3 className={style.header}>Appointments</h3>
+      </div>
+      <div className={style.searchContainer}>
+        <div>
+          <select name="" id=""></select>
+        </div>
+        <input
+          onChange={handleSearch}
+          type="text"
+          placeholder="Enter name to search"
+        />
+        <input type="text" />
+        <button onClick={() => openModal(MODAL_TYPES.TYPE4)} type="submit">
+          Add Appointment
+        </button>
+      </div>
+      <Table
+        headers={table}
+        data={data}
+        itemsPerPage={5}
+        renderRow={renderRow}
+      />
+    </div>
+  );
 }
 
-export default Appointments;
+export default Organizations;
--- src/pages/app/Organization/Organizations.jsx
+++ src/pages/app/Organization/Organizations.jsx
@@ -5,38 +5,11 @@ import { MODAL_TYPES, useModal } from "../../../context/ModalContext";
 import ViewIcon from "../../../assets/ViewIcon";
 import EditIcon from "../../../assets/EditIcon";
 import DeleteIcon from "../../../assets/DeleteIcon";
+import Table from "../../../components/dataTable/Table";
 
 function Organizations() {
   const [data, setData] = useState(initialData);
   const { openModal } = useModal();
-
-  const [currentPage, setCurrentPage] = useState(1);
-  const itemsPerPage = 5;
-  const totalPages = Math.ceil(data.length / itemsPerPage);
-
-  const indexOfLastItem = currentPage * itemsPerPage;
-  const indexOfFirstItem = indexOfLastItem - itemsPerPage;
-  const currentItems = data.slice(indexOfFirstItem, indexOfLastItem);
-
-  const handlePageChange = (pageNumber) => {
-    if (pageNumber >= 1 && pageNumber <= totalPages) {
-      console.log("clicked");
-      setCurrentPage(pageNumber);
-    }
-  };
-
-  const handleEdit = (item) => {
-    openModal(MODAL_TYPES.TYPE5, item);
-    console.log("Edit action for id:", item);
-  };
-
-  const handleView = (id) => {
-    console.log("View action for id:", id);
-  };
-
-  const handleDelete = (id) => {
-    console.log("Delete action for id:", id);
-  };
 
   const handleSearch = (event) => {
     const filteredData = data.filter((item) =>
@@ -61,79 +34,7 @@ function Organizations() {
           Add organisation
         </button>
       </div>
-      <table className={style.table}>
-        <thead>
-          <tr>
-            {table.map((header, index) => (
-              <td key={index}>{header}</td>
-            ))}
-          </tr>
-        </thead>
-        <tbody>
-          {currentItems.map((item) => (
-            <tr key={item.id}>
-              <td>{item.name}</td>
-              <td>{item.username}</td>
-              <td>{item.mobileNumber}</td>
-              <td>{item.address}</td>
-              <td>{item.city}</td>
-              <td>{item.state}</td>
-              <td>{item.zipCode}</td>
-              <td className={style.buttonRow}>
-                <button
-                  onClick={() => handleView(item.id)}
-                  className={style.viewButton}
-                >
-                  <ViewIcon />
-                </button>
-                <button
-                  onClick={() => handleEdit(item)}
-                  className={style.editButton}
-                >
-                  <EditIcon />
-                </button>
-                <button
-                  onClick={() => handleDelete(item.id)}
-                  className={style.deleteButton}
-                >
-                  <DeleteIcon />
-                </button>
-              </td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
-      <div className={style.pagination}>
-        <button
-          onClick={() => handlePageChange(1)}
-          disabled={currentPage === 1}
-          className={style.pageButton}
-        >
-          First
-        </button>
-        <button
-          onClick={() => handlePageChange(currentPage - 1)}
-          disabled={currentPage === 1}
-          className={style.pageNextButton}
-        >
-          Previous
-        </button>
-
-        <button
-          onClick={() => handlePageChange(currentPage + 1)}
-          disabled={currentPage === totalPages}
-          className={style.pageNextButton}
-        >
-          Next
-        </button>
-        <button
-          onClick={() => handlePageChange(totalPages)}
-          disabled={currentPage === totalPages}
-          className={style.pageButton}
-        >
-          Last
-        </button>
-      </div>
+      <Table headers={table} data={data} modalType={MODAL_TYPES.TYPE4} />
     </div>
   );
 }
