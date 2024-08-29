52cb72380938810824e5dd9f8c5cc8d3e1c23613
marvellous <comlaf2001@gmail.com>
2024-08-29 16:52:02 +0100

    select element converted to component

--- src/pages/app/Staff/Staff.jsx
+++ src/pages/app/Staff/Staff.jsx
@@ -10,9 +10,50 @@ import { MODAL_TYPES } from '../../../context/ModalContext';
 import Button from '../../../components/Button/Button';
 import AddStaff from '../../../modals/staffModals/AddStaff';
 import Table2 from '../../../components/dataTable2/Table2';
+import SelectFilter from '../../../components/SelectFilter';
 
 
 const Staff = () => {
+
+  const item = [
+    {
+      specialization: 'Select Specialization',
+      value: ''
+    },
+    {
+      specialization: 'Surgeon',
+      value: 'Surgeon'
+    },
+    {
+      specialization: 'Dentist',
+      value: 'Dentist'
+    },
+    {
+      specialization: 'Opthamologist',
+      value: 'Opthamologist'
+    }
+    
+  ]
+  
+  const item2 = [
+    {
+      specialization: 'Select Practice',
+      value: ''
+    },
+    {
+      specialization: 'Surgeon',
+      value: 'Surgeon'
+    },
+    {
+      specialization: 'Dentist',
+      value: 'Dentist'
+    },
+    {
+      specialization: 'Opthamologist',
+      value: 'Opthamologist'
+    }
+    
+  ]
 
   const [showForm, setShowForm] = useState(false);
   const [data, setData] = useState(tableData);
@@ -67,28 +108,8 @@ const Staff = () => {
       <div className={style.info}>
         <div >
           <input type="text" placeholder='Name' className={style.filter} onChange={handleSearch}/> 
-          <select
-            name="Specialization"
-            className={style.filter}
-            value={specializationFilter}
-            onChange={handleSpecializationChange}
-          >
-            <option value="">Select Specialization</option>
-            <option value="Dentist">Dentist</option>
-            <option value="Ophthalmologist">Ophthalmologist</option>
-            <option value="Dermatologist">Dermatologist</option>
-          </select>
-          <select
-            name="Practice"
-            className={style.filter}
-            value={practiceFilter}
-            onChange={handlePracticeChange}
-          >
-            <option value="">Select Practice</option>
-            <option value="Dentist">Dentist</option>
-            <option value="Ophthalmologist">Ophthalmologist</option>
-            <option value="Dermatologist">Dermatologist</option>
-          </select>
+          <SelectFilter onChange={handleSpecializationChange} data={item} Filter={specializationFilter}/>
+          <SelectFilter onChange={handleSpecializationChange} data={item2} Filter={practiceFilter}/>
         </div>
         <Table2 data={data} Role={'Specialization'}/>
         {/* <Table headers={tableHeader} data={tableData} itemsPerPage={6} renderRow={renderRow} modalType={MODAL_TYPES.TYPE4}/> */}
