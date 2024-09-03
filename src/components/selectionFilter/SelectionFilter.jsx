6e77f94ecf475cf2bf81f815c22887d97e8a4644
eedreese <idbalorg@gmail.com>
2024-09-03 05:31:45 +0100

    remove commented out codes

--- src/components/selectionFilter/SelectionFilter.jsx
+++ src/components/selectionFilter/SelectionFilter.jsx
@@ -2,31 +2,26 @@ import React, { useState, useMemo, useCallback, useEffect } from "react";
 import style from "./selectStyle.module.css";
 
 const SelectionFilter = ({ data, setData, field, query }) => {
-  // Memoize unique values to avoid unnecessary recalculations
   const uniqueValues = useMemo(() => {
     const values = [...new Set(data.map((item) => item[field]))];
     return values;
   }, [data, field]);
 
-  // Keep the original data in a state
   const [originalData] = useState(data);
   const [selectedValue, setSelectedValue] = useState(query);
 
-  // Memoize the handleChange function
   const handleChange = useCallback((e) => {
     setSelectedValue(e.target.value);
   }, []);
 
   useEffect(() => {
-    // Filter data based on selected value
     const filteredData =
       selectedValue === query
         ? originalData
         : originalData.filter((item) => item[field] === selectedValue);
 
-    // Update data with filtered data
     setData(filteredData);
-  }, [selectedValue, field, setData, originalData]); // Include originalData
+  }, [selectedValue, field, setData, originalData]);
 
   return (
     <select
