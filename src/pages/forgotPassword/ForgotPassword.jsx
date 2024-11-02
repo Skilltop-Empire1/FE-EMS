fbe49dc593252e5584c833c07d186c0e4620989a
eedreese <idbalorg@gmail.com>
2024-11-02 20:17:57 +0100

    resolving import conflicts

--- src/pages/forgotPassword/ForgotPassword.jsx
+++ src/pages/forgotPassword/ForgotPassword.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import forgot from '@src/components/forgot2.png'
+import forgot from "../../components/forgot2.png";
 
 const ForgotPassword = () => {
   const [email, setEmail] = useState("");
@@ -38,16 +38,16 @@ const ForgotPassword = () => {
 
   return (
     <div className={`forgot-password flex h-screen items-center`}>
-
       <div className="w-1/2 flex justify-center items-center">
         <img src={forgot} alt="" />
       </div>
 
-
       <div className="w-1/2 flex justify-center items-center flex-col h-full gap-10 bg-[#E0F7F7B2]">
         <h2 className="text-6xl  text-[#333333]">Forgot Password?</h2>
-        <p className="text-xl text-[#333333]">Enter your email to set a new password.</p>
-        <form onSubmit={handleSubmit} className=' flex flex-col gap-3 w-4/5'>
+        <p className="text-xl text-[#333333]">
+          Enter your email to set a new password.
+        </p>
+        <form onSubmit={handleSubmit} className=" flex flex-col gap-3 w-4/5">
           <div className="w-full">
             <label className="text-lg text-[#333333]">Email</label>
             <br />
@@ -59,14 +59,17 @@ const ForgotPassword = () => {
               placeholder="Enter email"
               className="w-full h-12 rounded-md border-2 text-[#333333] bg-[#E0F7F7B2] border-[#33333380] p-4"
             />
-          {error && <p className="error">{error}</p>}
-          {message && <p className="message">{message}</p>}
+            {error && <p className="error">{error}</p>}
+            {message && <p className="message">{message}</p>}
           </div>
-          <button type="submit" className="mt-10 py-2 bg-blue-600 w-full rounded-md text-white">Send Reset Link</button>
+          <button
+            type="submit"
+            className="mt-10 py-2 bg-blue-600 w-full rounded-md text-white"
+          >
+            Send Reset Link
+          </button>
         </form>
       </div>
-
-
     </div>
   );
 };
--- src/pages/resetPassword/ResetPassword.jsx
+++ src/pages/resetPassword/ResetPassword.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import styles from "./ResetPassword.module.css";
-import forgot from '@src/components/reset.png'
+import forgot from "../../components/reset.png";
 
 function ResetPassword() {
   const [email, setEmail] = useState("");
@@ -37,66 +37,71 @@ function ResetPassword() {
 
   return (
     <div className={styles.container}>
-       <div className={`forgot-password flex h-screen items-center`}>
+      <div className={`forgot-password flex h-screen items-center`}>
+        <div className="w-1/2 flex justify-center items-center">
+          <img src={forgot} alt="" />
+        </div>
 
-<div className="w-1/2 flex justify-center items-center">
-  <img src={forgot} alt="" />
-</div>
-
-
-<div className="w-1/2 flex justify-center items-center flex-col h-full gap-10 bg-[#E0F7F7B2]">
-  <h2 className="text-6xl text-[#333333]">Reset your password</h2>
-  <p className="text-xl text-[#333333]">Enter your email address below to create a new password.</p>
-  <form onSubmit={handleSubmit} className=' flex flex-col gap-3 w-4/5'>
-    <div className="w-full">
-     <div className={styles.formGroup}>
-      <label className="text-lg text-[#333333]">Email</label>
-        <br />
-        <input
-          type="email"
-          id="email"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-          required
-          placeholder="Enter email"
-          className="w-full h-12 rounded-md border-2 text-[#333333] border-[#33333380] bg-[#E0F7F7B2] p-4"
-        /> 
-     </div>
-      <div className={styles.formGroup}>
-          <label className="text-lg text-[#333333]">Password</label>
-          <br />
-          <input
-             type="password"
-             id="newPassword"
-             value={newPassword}
-             onChange={(e) => setNewPassword(e.target.value)}
-            required
-            placeholder="Enter email"
-            className="w-full h-12 text-[#333333] rounded-md border-2 border-[#33333380] bg-[#E0F7F7B2] p-4"
-          />
-      </div >
-       <div className={styles.formGroup}>
-          <label className="text-lg text-[#333333]">Confirm password</label>
-          <br />
-          <input
-            type="password"
-            id="confirmPassword"
-            value={confirmPassword}
-            onChange={(e) => setConfirmPassword(e.target.value)}
-            required
-            placeholder="Enter email"
-            className="w-full h-12 text-[#333333] rounded-md border-2 border-[#33333380] bg-[#E0F7F7B2] p-4"
-          />
-       </div>
-      {error && <p className={styles.error}>{error}</p>}
-      {success && <p>Password has been reset successfully!</p>}
-    </div>
-    <button type="submit" className="mt-10 py-2 bg-blue-600 w-full rounded-md text-white">Reset password</button>
-  </form>
-</div>
-
-
-</div>
+        <div className="w-1/2 flex justify-center items-center flex-col h-full gap-10 bg-[#E0F7F7B2]">
+          <h2 className="text-6xl text-[#333333]">Reset your password</h2>
+          <p className="text-xl text-[#333333]">
+            Enter your email address below to create a new password.
+          </p>
+          <form onSubmit={handleSubmit} className=" flex flex-col gap-3 w-4/5">
+            <div className="w-full">
+              <div className={styles.formGroup}>
+                <label className="text-lg text-[#333333]">Email</label>
+                <br />
+                <input
+                  type="email"
+                  id="email"
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                  required
+                  placeholder="Enter email"
+                  className="w-full h-12 rounded-md border-2 text-[#333333] border-[#33333380] bg-[#E0F7F7B2] p-4"
+                />
+              </div>
+              <div className={styles.formGroup}>
+                <label className="text-lg text-[#333333]">Password</label>
+                <br />
+                <input
+                  type="password"
+                  id="newPassword"
+                  value={newPassword}
+                  onChange={(e) => setNewPassword(e.target.value)}
+                  required
+                  placeholder="Enter email"
+                  className="w-full h-12 text-[#333333] rounded-md border-2 border-[#33333380] bg-[#E0F7F7B2] p-4"
+                />
+              </div>
+              <div className={styles.formGroup}>
+                <label className="text-lg text-[#333333]">
+                  Confirm password
+                </label>
+                <br />
+                <input
+                  type="password"
+                  id="confirmPassword"
+                  value={confirmPassword}
+                  onChange={(e) => setConfirmPassword(e.target.value)}
+                  required
+                  placeholder="Enter email"
+                  className="w-full h-12 text-[#333333] rounded-md border-2 border-[#33333380] bg-[#E0F7F7B2] p-4"
+                />
+              </div>
+              {error && <p className={styles.error}>{error}</p>}
+              {success && <p>Password has been reset successfully!</p>}
+            </div>
+            <button
+              type="submit"
+              className="mt-10 py-2 bg-blue-600 w-full rounded-md text-white"
+            >
+              Reset password
+            </button>
+          </form>
+        </div>
+      </div>
       {/* <h2>Reset Password</h2>
       {error && <p className={styles.error}>{error}</p>}
       {success && <p>Password has been reset successfully!</p>}
